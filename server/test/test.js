029ebdeb27e808d591e0a7e9a3f7c776b6c43754
elena <hellnellt@gmail.com>
2020-09-19 11:33:07 +0200

    build: add dummy test top fix the build

---
+++ server/test/test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+
+describe('Dummy test', () => {
+    it('should return 2', () => {
+        expect(1 + 1).to.equal(2);
+    });
+    it('should return 9', () => {
+        expect(3 * 3).to.equal(9);
+    });
+});
